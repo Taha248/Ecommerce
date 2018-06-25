e6b288009d5c6621e12061793fd69eb3405204b5
Taha248 <Taha24888@gmail.com>
2018-06-25 12:12:30 +0500

    25

--- CheckOut.php
+++ CheckOut.php
@@ -2,7 +2,90 @@
 
 include_once('libraries.php');
 include_once('Variables.php');
+include_once('common/connection.php');
+
+
 $cart = $jcart->get_contents();
+ $sql = "SELECT * FROM BankInfo";
+$result = $con->query($sql);
+
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    $i=0;
+    while($row = $result->fetch_assoc()) {
+        $ACCOUNT_TITLE= $row['AccountTitle'];
+        $BANKNAME=$row['BankName'];
+        $BRANCHNAME=$row['BranchName'];
+        $BRANCHCODE=$row['BranchCode'];
+        $ACCOUNT_NUMBER=$row['AccountNumber'];
+        $BANKCITY=$row['BankCity'];
+        $SWIFT_CODE=$row['SwiftCode'];
+        
+    }
+} else {
+    echo "Product Brand NOT FOUND";
+}
+
+
+
+
+if (isset($_POST['first_name'])) {
+
+$id =InsertUserDetails($_POST['first_name'],$_POST['last_name'],$_POST['country'],
+                     $_POST['city'],$_POST['state'],$_POST['zip_code'],
+                     $_POST['address'],$_POST['phone_number'],$_POST['email_address']); 
+    foreach($cart as $item)
+                    {
+                      
+                    InsertOrder($item['id'],$item['qty'],$id);
+                    }
+$to      = 'nobody@example.com';
+$subject = 'the subject';
+$message = 'hello';
+$headers = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+}
+
+
+
+function InsertOrder($productID,$productQuantity,$UserDetailID){
+        global $con;
+    $sql = "INSERT INTO OrderDetails (productQuantity, productID, UserDetailID) VALUES('".$productID."','".$productQuantity."','".$UserDetailID."');";
+    
+  
+    $last_id=0;
+if (mysqli_query($con, $sql)) {
+    $last_id = mysqli_insert_id($con);
+    echo "New record created successfully.";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($con);
+}
+    
+}
+
+
+function InsertUserDetails($first_name,$last_name,$country,$city,$state,$zipcode,$address,$contact,$email){
+    global $con;
+    $sql = "INSERT INTO UserDetails (FirstName, LastName, Address,Country,City,State,ZipCode,Contact,emailAddress) VALUES('".$first_name."','".$last_name."','".$country."',
+            '".$city."','".$state."','".$zipcode."',
+            '".$address."','".$contact."','".$email."');";
+    
+  
+    $last_id=0;
+if (mysqli_query($con, $sql)) {
+    $last_id = mysqli_insert_id($con);
+    echo "New record created successfully. Last inserted ID is: " . $last_id;
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($con);
+}
+
+   return $last_id;
+}
+
 //echo $jcart->gettotal();
 
 function getProductImage($id,$con){
@@ -47,6 +130,7 @@ if ($resultBrand->num_rows > 0) {
 
 ?>
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <style>
     include_once('css/index.css');
 
@@ -54,7 +138,7 @@ if ($resultBrand->num_rows > 0) {
 </head>
 <body>
 
-<div class="container cont" style="margin-bottom:50px;">
+<div class="container cont" style="">
 
 	
 			<h2>Place <b>Order</b></h2>
@@ -78,9 +162,9 @@ if ($resultBrand->num_rows > 0) {
                 </div>
             </div>    
             <div class="row cart-body">
-                
+                <form class="form-horizontal" method="post" action="">
                     <?php
-                echo '<form class="form-horizontal" method="post" action="">
+                echo '
                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 col-md-push-6 col-sm-push-6">
                     <!--REVIEW ORDER-->
                     <div class="panel panel-info">
@@ -117,7 +201,9 @@ if ($resultBrand->num_rows > 0) {
                         </div>
                     </div>'?>
                     <!--REVIEW ORDER END-->
+                </form>
                 </div>
+                <form class="form-horizontal" method="post" action="">
                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 col-md-pull-6 col-sm-pull-6">
                     <!--SHIPPING METHOD-->
                     <div class="panel panel-info">
@@ -185,75 +271,33 @@ if ($resultBrand->num_rows > 0) {
                         <div class="panel-heading"><span><i class="glyphicon glyphicon-lock"></i></span> Secure Payment</div>
                         <div class="panel-body">
                             <div class="form-group">
-                                <div class="col-md-12"><strong>Card Type:</strong></div>
+                                <div class="col-md-12"><strong>Account Title :</strong></div>
                                 <div class="col-md-12">
-                                    <select id="CreditCardType" name="CreditCardType" class="form-control">
-                                        <option value="5">Visa</option>
-                                        <option value="6">MasterCard</option>
-                                        <option value="7">American Express</option>
-                                        <option value="8">Discover</option>
-                                    </select>
+                                 <p> <?php echo $ACCOUNT_TITLE; ?></p>
                                 </div>
+                            </div>
+        
+                            <div class="form-group">
+                                <div class="col-md-12"><strong>Branch Name:    </strong> 
+                               <?php echo $BANKNAME; ?></div>
+                               
                             </div>
                             <div class="form-group">
-                                <div class="col-md-12"><strong>Credit Card Number:</strong></div>
-                                <div class="col-md-12"><input type="text" class="form-control" name="car_number" value="" /></div>
+                                <div class="col-md-12"><strong>Branch Code: </strong>
+                               <?php echo $BRANCHCODE; ?></div>
                             </div>
-                            <div class="form-group">
-                                <div class="col-md-12"><strong>Card CVV:</strong></div>
-                                <div class="col-md-12"><input type="text" class="form-control" name="car_code" value="" /></div>
+                              <div class="form-group">
+                                <div class="col-md-12"><strong>Account#:</strong> <?php echo $ACCOUNT_NUMBER; ?></div>
                             </div>
-                            <div class="form-group">
-                                <div class="col-md-12">
-                                    <strong>Expiration Date</strong>
-                                </div>
-                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                    <select class="form-control" name="">
-                                        <option value="">Month</option>
-                                        <option value="01">01</option>
-                                        <option value="02">02</option>
-                                        <option value="03">03</option>
-                                        <option value="04">04</option>
-                                        <option value="05">05</option>
-                                        <option value="06">06</option>
-                                        <option value="07">07</option>
-                                        <option value="08">08</option>
-                                        <option value="09">09</option>
-                                        <option value="10">10</option>
-                                        <option value="11">11</option>
-                                        <option value="12">12</option>
-                                </select>
-                                </div>
-                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                    <select class="form-control" name="">
-                                        <option value="">Year</option>
-                                        <option value="2015">2015</option>
-                                        <option value="2016">2016</option>
-                                        <option value="2017">2017</option>
-                                        <option value="2018">2018</option>
-                                        <option value="2019">2019</option>
-                                        <option value="2020">2020</option>
-                                        <option value="2021">2021</option>
-                                        <option value="2022">2022</option>
-                                        <option value="2023">2023</option>
-                                        <option value="2024">2024</option>
-                                        <option value="2025">2025</option>
-                                </select>
-                                </div>
+                              <div class="form-group">
+                                <div class="col-md-12"><strong>Branch City:</strong>
+                            <?php echo $BANKCITY; ?></div>
                             </div>
-                            <div class="form-group">
-                                <div class="col-md-12">
-                                    <span>Pay secure using your credit card.</span>
-                                </div>
-                                <div class="col-md-12">
-                                    <ul class="cards">
-                                        <li class="visa hand">Visa</li>
-                                        <li class="mastercard hand">MasterCard</li>
-                                        <li class="amex hand">Amex</li>
-                                    </ul>
-                                    <div class="clearfix"></div>
-                                </div>
+                              <div class="form-group">
+                                <div class="col-md-12"><strong>Swift Code:</strong><?php echo $SWIFT_CODE; ?></div>
+                               <p> </p>
                             </div>
+                            
                             <div class="form-group">
                                 <div class="col-md-6 col-sm-6 col-xs-12">
                                     <button type="submit" class="btn btn-primary btn-submit-fix">Place Order</button>
@@ -263,15 +307,10 @@ if ($resultBrand->num_rows > 0) {
                     </div>
                     <!--CREDIT CART PAYMENT END-->
                 </div>
-                
                 </form>
-            </div>
-            <div class="row cart-footer">
-        
             </div>
     </div>
 </div>
 
-</div>
     <?php include_once('footer.php');  ?>
 </body>
--- ViewCart.php
+++ ViewCart.php
@@ -71,6 +71,7 @@ function CheckOut(){
 
 ?>
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <style>
     include_once('css/index.css');
 
